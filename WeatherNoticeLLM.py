d8f80d28e38f15a0c5d69c4ff3e49e8d840d50f2
junans0boi <105141262+junans0boi@users.noreply.github.com>
2024-05-23 19:24:41 +0900

    GTTS and LLM basic settings

---
+++ WeatherNoticeLLM.py
@@ -0,0 +1,15 @@
+import getpass
+import os
+
+os.environ["GOOGLE_API_KEY"] = "AIzaSyBZfOhQ0aVyQllnUIGB4KbTlUZZRVE8mVQ"
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+
+model = ChatGoogleGenerativeAI(model="gemini-pro")
+output_parser = StrOutputParser()
+
+chain = ChatGoogleGenerativeAI(model="gemini-pro") | output_parser
+
+dick = chain.invoke("응~니얼굴~")
+print(dick)
